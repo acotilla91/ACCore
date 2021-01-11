a88c0352d1eedb48295cfe2ca8296d79b1c83d8e
Alejandro Cotilla <acotilla91@gmail.com>
2021-01-11 15:45:19 -0500

    Make the `preferredStatusBarStyle` extension only available for iOS.

--- ACCore/Extensions/UINavigationController+ACExtensions.swift
+++ ACCore/Extensions/UINavigationController+ACExtensions.swift
@@ -9,9 +9,11 @@ import UIKit
 
 extension UINavigationController {
     
+    #if os(iOS)
     /// Make UINavigationController update the status bar style based on their top level controller.
     /// Based on: https://medium.com/swiftindia/status-bar-throwing-tantrums-in-ios-9-ed567e3a8f3b
     open override var preferredStatusBarStyle: UIStatusBarStyle {
         return topViewController?.preferredStatusBarStyle ?? .default
     }
+    #endif
 }
