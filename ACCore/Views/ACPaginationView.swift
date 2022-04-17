f4115da7cdc332d8b70b598efd2b5dee36ccf007
Alejandro Cotilla <acotilla91@gmail.com>
2022-04-17 18:54:20 -0400

    Fix deprecation warning.

--- ACCore/Views/ACPaginationView.swift
+++ ACCore/Views/ACPaginationView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-protocol ACPaginationViewDelegate: class {
+protocol ACPaginationViewDelegate: AnyObject {
     func paginationView(_ paginationView: ACPaginationView, didSelect index: Int)
 }
 
